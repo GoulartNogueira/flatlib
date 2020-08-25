5108d0c4366f679e885cecb9e1a6ab1dacd39641
André Goulart Nogueira <goulartnogueira@gmail.com>
2020-08-25 02:38:16 -0300

    add my code

---
+++ astro.py
@@ -0,0 +1,28 @@
+from http.server import BaseHTTPRequestHandler
+
+from flatlib import const
+from flatlib.chart import Chart
+from flatlib.datetime import Datetime
+from flatlib.geopos import GeoPos
+
+date = Datetime('1929/01/15', '12:00', '-06:00')
+pos = GeoPos('33n45:01', '84w23:01')
+
+chart = Chart(date, pos)
+sun = chart.get(const.SUN)
+print(chart.get(const.SUN),chart.get(const.MOON),chart.get(const.ASC))
+
+message = []
+for obj in chart.objects:
+    message.append(obj)
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write(message.encode())
+        return
+
+
