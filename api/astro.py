7aab3c278691b59f54f3b075ecae0faedf4c083d
Andre Goulart <goulartnogueira@gmail.com>
2020-09-02 00:27:32 -0300

    self.send_header('Content-type', 'application/json')

--- api/astro.py
+++ api/astro.py
@@ -92,9 +92,9 @@ class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write(astro)
+        self.wfile.write(json.dumps(astro))
         return
 
 import logging
--- api/astro_local.py
+++ api/astro_local.py
@@ -106,7 +106,7 @@ class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
         self.wfile.write(astro)
         return
