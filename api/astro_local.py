f656cc8a9c265c58e9d2027def0a376b261f0d39
Andre Goulart <goulartnogueira@gmail.com>
2020-09-01 22:57:18 -0300

    pip install pyswisseph==2.00.00-2

---
+++ api/astro_local.py
@@ -0,0 +1,18 @@
+
+from flatlib import const
+from flatlib.chart import Chart
+from flatlib.datetime import Datetime
+from flatlib.geopos import GeoPos
+
+date = Datetime('1929/01/15', '12:00', '-06:00')
+pos = GeoPos('33n45:01', '84w23:01')
+
+chart = Chart(date, pos)
+sun = chart.get(const.SUN)
+print(chart.get(const.SUN),chart.get(const.MOON),chart.get(const.ASC))
+
+message = []
+for obj in chart.objects:
+    message.append(obj)
+
+print(message)
