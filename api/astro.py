2294d8af019fbbe64f470ec1a5dd3f2d8315a87e
Andre Goulart <goulartnogueira@gmail.com>
2020-09-07 20:50:35 -0300

    astro_local

--- api/astro.py
+++ api/astro.py
@@ -4,98 +4,111 @@ from flatlib import const
 from flatlib.chart import Chart
 from flatlib.datetime import Datetime
 from flatlib.geopos import GeoPos
+import dateutil.parser
+try:
+    import urlparse
+except ImportError:
+    import urllib.parse as urlparse
+    
 import json
 import numpy as np
 
 def angle_dif(a,b):
-    dif = (b-a+360)%360
-    return(dif)
+	dif = (b-a+360)%360
+	return(dif)
 
 
 def get_astrological(date,coordinates,timezone=''):
-    if isinstance(coordinates, (str,)):
-        if "," in coordinates:
-            coord = coordinates.split(',')
-            coord[0]=coord[0].lstrip()
-            coord[1]=coord[1].lstrip()
-           # if len(coord[1])>2:
-            #    coord[1] = coord[1][:1]+":" +coord[1][2:]
-        #        print(coord[1])
-    
-    elif isinstance(coordinates, (list,)):
-        if len(coordinates) != 2:
-            return()
-        coord = coordinates
-    else:
-        return()
+	if isinstance(coordinates, (str,)):
+		if "," in coordinates:
+			coord = coordinates.split(',')
+			coord[0]=coord[0].lstrip()
+			coord[1]=coord[1].lstrip()
+			# if len(coord[1])>2:
+			#	coord[1] = coord[1][:1]+":" +coord[1][2:]
+		#		print(coord[1])
+	
+	elif isinstance(coordinates, (list,)):
+		if len(coordinates) != 2:
+			return()
+		coord = coordinates
+	else:
+		return()
 
-    pos = GeoPos(coord[0],coord[1])
-    
-    if timezone == '' or timezone.startswith("m"):
-        return()
-    #if isinstance(timezone, (str,)):
-    #    date = Datetime(date.strftime("%Y/%m/%d"), date.strftime('%H:%M'), timezone)
-        timezone = str(get_timezone(pos.lat, pos.lon,date))
-        print(timezone)
-        #print(date)
-    #else:
-        #date += timezone
-    date = Datetime(date.strftime("%Y/%m/%d"), date.strftime('%H:%M'), timezone)
-    #print(date)
-    
-    
-    
-    chart = Chart(date, pos)
+	pos = GeoPos(coord[0],coord[1])
+	
+	if timezone == '' or timezone.startswith("m"):
+		return()
+	#if isinstance(timezone, (str,)):
+	#	date = Datetime(date.strftime("%Y/%m/%d"), date.strftime('%H:%M'), timezone)
+		timezone = str(get_timezone(pos.lat, pos.lon,date))
+		print(timezone)
+		#print(date)
+	#else:
+		#date += timezone
+	date = Datetime(date.strftime("%Y/%m/%d"), date.strftime('%H:%M'), timezone)
+	#print(date)
+	
+	
+	
+	chart = Chart(date, pos)
 
-    astro = {}
-    for obj in [chart.get(const.ASC)]:
-    #    #print(obj)
-        astro[obj.id] = {'sign':obj.sign,'lon':obj.lon}
-    
-    for obj in chart.houses:
-        astro[obj.id] = {'sign':obj.sign,'lon':obj.lon,'signlon':obj.signlon,'size':30-obj.size}
+	astro = {}
+	for obj in [chart.get(const.ASC)]:
+	#	#print(obj)
+		astro[obj.id] = {'sign':obj.sign,'lon':obj.lon}
+	
+	for obj in chart.houses:
+		astro[obj.id] = {'sign':obj.sign,'lon':obj.lon,'signlon':obj.signlon,'size':30-obj.size}
 
-    for obj in chart.objects: #Planets
-        #print(obj.id,obj.sign,obj.lon,obj.signlon,obj.lonspeed)
-        astro[obj.id] = {'sign':obj.sign,'lon':obj.lon,'speed':obj.lonspeed}
-        try:
-            gender = obj.gender()
-            astro[obj.id].update({'gender':gender})
-        except:
-            pass
-        try: 
-            mean_motion = obj.meanMotion()
-            if mean_motion:
-                astro[obj.id].update({'speed':obj.lonspeed/mean_motion})
-        except: pass
-        try: astro[obj.id].update({'fast':str(obj.isFast())})
-        except: pass
-        for house in chart.houses:
-            if house.hasObject(obj):
-                astro[obj.id].update({'house':house.id})
-    moon_phase = angle_dif(astro['Moon']['lon'],astro['Sun']['lon'])
-    astro['Moon'].update({
-        'phase':moon_phase,
-        'phase_position':[np.sin(moon_phase* np.pi / 180.),np.cos(moon_phase* np.pi / 180.)]
-    })
-    ASC_LON = chart.get(const.ASC).lon
-    for obj in astro.keys():
-        if 'lon' in astro[obj].keys():
-            angle = angle_dif(ASC_LON,astro[obj]['lon'])
-            astro[obj].update({'lon':angle,'position':[np.sin(angle* np.pi / 180.),np.cos(angle* np.pi / 180.)]})
-    return(astro)
-
-import dateutil.parser
-astro = get_astrological(dateutil.parser.parse('1991-May-01 08:35AM'),[23.6713029,-46.5690634],"-03:00")
+	for obj in chart.objects: #Planets
+		#print(obj.id,obj.sign,obj.lon,obj.signlon,obj.lonspeed)
+		astro[obj.id] = {'sign':obj.sign,'lon':obj.lon,'speed':obj.lonspeed}
+		try:
+			gender = obj.gender()
+			astro[obj.id].update({'gender':gender})
+		except:
+			pass
+		try: 
+			mean_motion = obj.meanMotion()
+			if mean_motion:
+				astro[obj.id].update({'speed':obj.lonspeed/mean_motion})
+		except: pass
+		try: astro[obj.id].update({'fast':str(obj.isFast())})
+		except: pass
+		for house in chart.houses:
+			if house.hasObject(obj):
+				astro[obj.id].update({'house':house.id})
+	moon_phase = angle_dif(astro['Moon']['lon'],astro['Sun']['lon'])
+	astro['Moon'].update({
+		'phase':moon_phase,
+		'phase_position':[np.sin(moon_phase* np.pi / 180.),np.cos(moon_phase* np.pi / 180.)]
+	})
+	ASC_LON = chart.get(const.ASC).lon
+	for obj in astro.keys():
+		if 'lon' in astro[obj].keys():
+			angle = angle_dif(ASC_LON,astro[obj]['lon'])
+			astro[obj].update({'lon':angle,'position':[np.sin(angle* np.pi / 180.),np.cos(angle* np.pi / 180.)]})
+	return(astro)
 
 class handler(BaseHTTPRequestHandler):
+	datetime = '1991-May-01 08:35AM'
+	latlong = [23.6713029,-46.5690634]
+	fuso = "-03:00"
+	parsed_path = urlparse.urlparse(self.path)
+	if 'datetime' in parsed_path:
+		datetime = parsed_path['datetime']
+		print(datetime)
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        self.wfile.write(json.dumps(astro,ensure_ascii=False).encode('utf8'))
-        return
+	astro = get_astrological(dateutil.parser.parse(datetime),latlong,fuso)
+	#astro.update({"query":parsed_path.query})
+
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header('Content-type', 'application/json')
+		self.end_headers()
+		self.wfile.write(json.dumps(astro,ensure_ascii=False).encode('utf8'))
+		return
 
 import logging
 logging.debug('A debug message')
--- api/index2.py
+++ api/index2.py
@@ -1,12 +1,72 @@
 from http.server import BaseHTTPRequestHandler
 from cowpy import cow
 
-class handler(BaseHTTPRequestHandler):
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        message = cow.Cowacter().milk('MMMMMMMOOOOOOOOOOOOOrzão! <3')
-        self.wfile.write(message.encode())
-        return
+
+try:
+	import urlparse
+except ImportError:
+	import urllib.parse as urlparse
+
+class GetHandler(BaseHTTPRequestHandler):
+	
+	def do_GET(self):
+		parsed_path = urlparse.urlparse(self.path)
+
+		# message_parts = [
+		# 		'CLIENT VALUES:',
+		# 		'client_address=%s (%s)' % (self.client_address,
+		# 									self.address_string()),
+		# 		'command=%s' % self.command,
+		# 		'path=%s' % self.path,
+		# 		'real path=%s' % parsed_path.path,
+		# 		'query=%s' % parsed_path.query,
+		# 		'params=%s' % parsed_path.params,
+		# 		'request_version=%s' % self.request_version,
+		# 		'',
+		# 		'SERVER VALUES:',
+		# 		'server_version=%s' % self.server_version,
+		# 		'sys_version=%s' % self.sys_version,
+		# 		'protocol_version=%s' % self.protocol_version,
+		# 		'',
+		# 		'HEADERS RECEIVED:',
+		# 		]
+		# for name, value in sorted(self.headers.items()):
+		# 	message_parts.append('%s=%s' % (name, value.rstrip()))
+		# message_parts.append('')
+		# message = '\r\n'.join(message_parts)
+
+
+		self.send_response(200)
+		self.end_headers()
+
+		query = urlparse.parse_qs(parsed_path.query)
+		print(query)
+
+		message = 'MMMMMMMOOOOOOOOOOOOOrzão! <3'
+		if "message" in query:
+			message = str(query['message'][0])
+			print(message)
+		
+		message = cow.Cowacter().milk(message)
+		self.wfile.write(message.encode('utf8'))
+		return
+
+if __name__ == '__main__':
+	from http.server import BaseHTTPRequestHandler, HTTPServer
+	server = HTTPServer(('localhost', 8080), GetHandler)
+	print('Starting server, use <Ctrl-C> to stop')
+	server.serve_forever()
+
+
+
+
+# class handler(BaseHTTPRequestHandler):
+
+#	 def do_GET(self):
+#		 self.send_response(200)
+#		 self.send_header('Content-type','text/plain')
+#		 self.end_headers()
+#		 message = cow.Cowacter().milk('MMMMMMMOOOOOOOOOOOOOrzão! <3')
+#		 self.wfile.write(message.encode())
+#		 return
