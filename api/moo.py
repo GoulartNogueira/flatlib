b95d9e9c1b9e32f7af50e9cbf4a815a5588d9f42
Andre Goulart <goulartnogueira@gmail.com>
2020-10-16 10:18:37 -0300

    flask POST with json

--- api/moo.py
+++ api/moo.py
@@ -1,6 +1,6 @@
 from cowpy import cow
 
-from flask import Flask, Response, request
+from flask import Flask, Response, request, json
 app = Flask(__name__)
 
 # @app.route('/', defaults={'path': ''})
@@ -8,10 +8,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+	response = app.response_class(
+		response="Hello World!",
+		status=200,
+		mimetype='text'
+	)
+	return response
 
-@app.route('/<path>')
-@app.route('/<path>',methods=["POST"])
+# @app.route('/<path>')
+@app.route('/<path>',methods=["GET","POST"])
 
 def catch_all(path):
 	user_message = request.args.get('message', type = str)
@@ -20,7 +25,13 @@ def catch_all(path):
 		ans = cow.Cowacter().milk(user_message)
 	else:
 		ans = cow.Cowacter().milk(path)
-	return Response(ans, mimetype="text")
+	response = app.response_class(
+		response=json.dumps(ans),
+		status=200,
+		mimetype='application/json'
+	)
+	return response
 
 if __name__ == '__main__':
-    app.run()
+	app.debug = True
+	app.run()
