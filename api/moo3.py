eb7bc7d3084ef38b91509e3ed124d9d62864d9d9
Andre Goulart <goulartnogueira@gmail.com>
2020-10-16 13:14:40 -0300

    sanic API

---
+++ api/moo3.py
@@ -0,0 +1,23 @@
+from cowpy import cow
+from sanic import Sanic
+from sanic.response import json, text
+app = Sanic(__name__)
+
+
+@app.route('/')
+@app.route('/<path:path>',methods=["GET","POST"])
+async def index(request, path=""):
+	user_message = request.args.get('message')
+	print(user_message)
+	if user_message:
+		ans = cow.Cowacter().milk(user_message)
+	else:
+		ans = cow.Cowacter().milk(path)
+
+	response = json({'cow': ans})
+	response = text(ans)
+	return(response)
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run()
